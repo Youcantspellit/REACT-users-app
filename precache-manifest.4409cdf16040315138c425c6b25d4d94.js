59c95a232f5a8f9b63ec349d649dfb20cf5a5c1d
Ninja <silvia@nitro.fritz.box>
2019-04-01 10:44:40 +0200

    Updates

---
+++ precache-manifest.4409cdf16040315138c425c6b25d4d94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "270ee04f62cc99e25cee",
+    "url": "/react-users-app/static/css/main.9326ccb6.chunk.css"
+  },
+  {
+    "revision": "270ee04f62cc99e25cee",
+    "url": "/react-users-app/static/js/main.270ee04f.chunk.js"
+  },
+  {
+    "revision": "65453e92c3d74dd89173",
+    "url": "/react-users-app/static/js/runtime~main.65453e92.js"
+  },
+  {
+    "revision": "591719dd096d58f14a0c",
+    "url": "/react-users-app/static/js/2.591719dd.chunk.js"
+  },
+  {
+    "revision": "1da0b76bfc03cd909d5c46fc61a89c42",
+    "url": "/react-users-app/index.html"
+  }
+];
